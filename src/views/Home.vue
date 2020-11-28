1e152e51d8c9e7f1f2c88d400e7e9ce193f21266
robertobirijnr <robertobirijnr@hotmail.com>
2020-11-28 22:52:52 +0000

    navbar and contents

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,128 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <header>
+      <a href="#" ><img src="../assets/logo.png" class="logo" alt=""></a>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">What's New</a></li>
+        <li><a href="#">Contact us</a></li>
+      </ul>
+    </header>
+    <div class="content">
+      <div class="textbox">
+        <h2>It's not just Coffee <br>It's <span>Starbucks</span></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore blanditiis eligendi cupiditate odio eveniet iste libero error asperiores. Obcaecati vero, illo doloribus vitae pariatur laborum tempore? Eos minima placeat similique.</p>
+        <a href="#">Learn More</a>
+      </div>
+      <div class="imgBox">
+        <img src="../assets/img1.png" class="starbucks" alt="">
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    
   }
-}
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,200;1,300;1,400&display=swap');
+
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+section{
+  position: relative;
+  width: 100%;
+  min-width: 100vh;
+  padding: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+}
+header{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 20px 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header .logo{
+  position:relative;
+  max-width: 80px;
+}
+
+header ul{
+  position: relative;
+  display: flex;
+}
+
+header ul li{
+  list-style: none ;
+}
+
+header ul li a{
+  display: inline-block;
+  color: #333;
+  font-weight: 400;
+  margin-left: 40px;
+  text-decoration: none;
+}
+
+.content{
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.content .textbox{
+  position: relative;
+  max-width: 600px;
+}
+
+.content .textbox h2{
+  color:#333;
+  font-size: 4em;
+  line-height: 1.4em;
+  font-weight: 500;
+}
+
+.content .textbox h2 span{
+  color: #017143;
+  font-size: 1.2em;
+  font-weight: 900;
+}
+.content .textbox p{
+  color: #333;
+}
+.content .textbox a{
+  display: inline-block;
+  margin-top: 20px;
+  padding: 8px 20px;
+  background: #017143;
+  color: white;
+  border-radius: 40px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  text-decoration: none ;
+}
+.content .imgBox{
+  width: 600px;
+  display: flex;
+  justify-content: flex-start;
+}
+
+</style>
